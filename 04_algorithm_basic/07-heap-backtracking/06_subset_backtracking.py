1be6fce669e5b5d74cf7d570496c782b1782c1ed
박사랑 <sarangx1227@gmail.com>
2025-08-07 15:22:19 +0900

    UPDATE

--- 04_algorithm_basic/07-heap-backtracking/06_subset_backtracking.py
+++ 04_algorithm_basic/07-heap-backtracking/06_subset_backtracking.py
@@ -13,14 +13,16 @@ def find_subset(start, current_subset, current_sum):
         # result.append(current_subset[:]) XX
         result.append(list(current_subset))
         return
-    # start부터 전체 수를 다 순회
-    for idx in range(start, len(nums)):
+
+    # 'start부터' 전체 수를 다 순회
+    # 0부터 순회하면 다음 조사하러 갔을 때 이전 조사에서 선택 여부 골랐는데
+    # 다시 중복해서 선택하게 되버림(중복조합, 중복순열)
+   for idx in range(start, len(nums)):
         num = nums[idx]
         # 현재 선택한 수를 집합에 넣고, 값도 추가해서 다음 작업
         current_subset.append(num)
         find_subset(idx + 1, current_subset, current_sum + num)
         current_subset.pop()
-
 
 nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 target_sum = 10
@@ -28,7 +30,7 @@ result = []
 cnt = 0
 
 # 필요한 인자들은?
-# 1. 재귀를 중단시킬 파라미터 (총합이 10이 되면 종료)
+# 1. 재귀를 중단시킬 파라미터 (총합이 10이 되면 종료) -> 기준점
 # 2. 누적해서 가야할 파라미터 -> 만들어지는 부분집합
 # + 그 선택할 집합의 index 파라미터
 find_subset(start=0, current_subset=[], current_sum=0)
