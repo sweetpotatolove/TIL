e9d6e03d770e8bb181b1a171e6fb9bbf561b8852
박사랑 <sarangx1227@gmail.com>
2025-08-08 17:53:00 +0900

    UPDATE

---
+++ 04_algorithm_basic/my_hw/16546_Baby-gin/sol.py
@@ -0,0 +1,18 @@
+import sys
+sys.stdin = open('input.txt')
+
+T = int(input())
+for test_case in range(1, T+1):
+    cards = input()
+    print(cards)
+
+
+    for i in range(cards):
+
+
+
+
+
+
+
+
