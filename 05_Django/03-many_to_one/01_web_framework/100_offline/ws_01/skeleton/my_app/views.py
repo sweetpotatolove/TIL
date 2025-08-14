57adc218e86f29b29c4d43a13080c4108a59f077
박사랑 <sarangx1227@gmail.com>
2025-08-14 14:13:58 +0900

    UPDATE

--- 05_Django/03-many_to_one/01_web_framework/100_offline/ws_01/skeleton/my_app/views.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.decorators import api_view
-from django.http import JsonResponse
-
-# Create your views here.
-@api_view(['GET'])
-def hello(request):
-    return JsonResponse({'message': 'Welcome to your very first Django project'})
--- 05_Django/03-many_to_one/02_model_serializer/100_offline/ws_04/skeleton/patients/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-    path("", views.patient_list_create),
-    path("<int:patient_id>/", views.patient_detail)
-
-]
